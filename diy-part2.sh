96c150efad5eb83fc1e8f160881e812b30e679fe
caiweill <80924478+caiweill@users.noreply.github.com>
2025-02-04 22:34:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,19 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# 获取MosDNSluci-app-mosdns
+rm -rf feeds/packages/net/mosdns
+rm -rf package/feeds/packages/mosdns
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf package/feeds/packages/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+# 获取隔空播放luci-app-airconnect
+git clone https://github.com/sbwml/luci-app-airconnect package/airconnect
+
+## OpenClash
+git clone --depth 1 https://github.com/vernesong/openclash.git OpenClash
+rm -rf feeds/luci/applications/luci-app-openclash
+mv OpenClash/luci-app-openclash feeds/luci/applications/luci-app-openclash
